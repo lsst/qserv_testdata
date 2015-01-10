07b192a29ceb8ded2f2be53b8dcc5c2166d227c2
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2015-01-10 04:04:44 +0100

    Add draft version of data-download script

---
+++ datasets/case04/data/fetch-data.sh
@@ -0,0 +1,3 @@
+# TODO use http for non authenticated access
+# rsync -avzhe ssh lsst-dev.ncsa.illinois.edu:public_html/qserv_testdata/datasets/case04/data .
+rsync -avzhe ssh DeepSource.csv.gz lsst-dev.ncsa.illinois.edu:public_html/qserv_testdata/datasets/case04/data
