b1da1ff24dee1e3ee0e7eb53b563c3ebea11d778
Andy Salnikov <salnikov@slac.stanford.edu>
2017-11-27 13:41:18 -0800

    Fix angular separation for 1051_nn, must be smaller than overlap

--- datasets/case01/queries/1051_nn.sql
+++ datasets/case01/queries/1051_nn.sql
@@ -14,5 +14,5 @@ SELECT o1.objectId AS objId1,
  WHERE o1.ra_PS BETWEEN 0 AND 0.2 -- noQserv
    AND o1.decl_PS between 0 and 1 -- noQserv
 -- withQserv WHERE qserv_areaspec_box(0, 0, 0.2, 1)
-   AND scisql_angSep(o1.ra_PS, o1.decl_PS, o2.ra_PS, o2.decl_PS) < 0.05
+   AND scisql_angSep(o1.ra_PS, o1.decl_PS, o2.ra_PS, o2.decl_PS) < 0.016
    AND o1.objectId <> o2.objectId
