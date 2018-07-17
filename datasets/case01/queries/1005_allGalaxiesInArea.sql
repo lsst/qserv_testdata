33b9a97c36063eafc32dba4a4df45a034d812705
Nathan Pease <npease@slac.stanford.edu>
2018-07-17 11:25:40 -0700

    fix query
    replace variables with concrete values
    
    replace non-populated extendedParamater column
    with rRadius_SG to determine object size

--- datasets/case01/queries/1005_allGalaxiesInArea.sql
+++ datasets/case01/queries/1005_allGalaxiesInArea.sql
@@ -1,11 +1,11 @@
 -- Select all galaxies in a given area
 -- http://dev.lsstcorp.org/trac/wiki/dbQuery014
 
--- Missing in current schema: extendedParameter
+-- pragma sortresult
 
 SELECT objectId
 FROM   Object
-WHERE  ra_PS BETWEEN :raMin AND :raMax       -- noQserv
-   AND decl_PS BETWEEN :declMin AND :declMax -- noQserv
--- withQserv WHERE qserv_areaspec_box(:raMin, :declMin, :raMax, :declMax)
-AND    extendedParameter > 0.8
+WHERE  ra_PS BETWEEN 1.0 AND 2.0       -- noQserv
+   AND decl_PS BETWEEN -6.0 AND -4.0 -- noQserv
+-- withQserv WHERE qserv_areaspec_box(1.0, -6.0, 2.0, -4.0)
+AND    rRadius_SG > 0.5
